da2032259bffd8313308167aab181c0a3807f9e7
Cyber-Wizard-Dev <raizzler.dev@gmail.com>
2025-02-21 12:27:10 -0800

    Add files via upload

---
+++ EntangleArrayBug-master/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <livewire:styles />
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    </head>
+    <body class="antialiased">
+
+        <livewire:list-test />
+
+        <livewire:scripts />
+    </body>
+</html>
